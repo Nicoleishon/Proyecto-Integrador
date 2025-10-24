caa99bc006f8ce5c7ee83a2c09232a7c20bcacc7
Nico <nicocontrera200@gmail.com>
2025-10-24 15:09:57 -0700

    actualizado la clase turno

--- ProyectoIntegrador/src/main/java/com/mycompany/proyectointegrador/Turno.java
+++ ProyectoIntegrador/src/main/java/com/mycompany/proyectointegrador/Turno.java
@@ -1,17 +1,24 @@
-
 package com.mycompany.proyectointegrador;
-
+import java.util.Date;
 
 public class Turno {
-    private String turnoId;
-    private String fechaHora;
+    private String idTurno;
+    private Date fechaHora;
     private String tipoConsulta;
+    private EstadoTurno estado;
+    private String motivoConsulta;
+    private Medico medico;
+    private Paciente paciente;
 
-    public Turno(String turnoId, String fechaHora, String tipoConsulta) {
-        this.turnoId = turnoId;
+    public Turno(String turnoId, Date fechaHora, String tipoConsulta, EstadoTurno estado) {
+        this.idTurno = idTurno;
         this.fechaHora = fechaHora;
         this.tipoConsulta = tipoConsulta;
+        this.estado = estado;
     }
     
+    public void cambiarEstadoTurno(EstadoTurno nuevoEstado){
+        this.estado = nuevoEstado;
+    }
     
 }
